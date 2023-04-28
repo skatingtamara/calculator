662443b67635a0302466bece31467f948aa118f9
skatingtamara <98510821+skatingtamara@users.noreply.github.com>
2023-04-28 13:33:48 -0700

    beginning JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-// console.log('hello')
+//display() function wil display the value of the clicked buttons
+
+function display(clicked){
+    // get something to shopw up in  the display-box
+        
+    document.querySelector('.display-box').value = clicked
+    
+    // place the clicked button in the display-box and keep it there
+    
+    // place another clickck item in the display box
+
+
+}
+
+
+
+
+
+
+// calculate() function will evaluate the entered expression
+
+
+
+// clearLast() function will delete the last entered value
+
+
+// clearScreen() function will clear all on the screen
+
